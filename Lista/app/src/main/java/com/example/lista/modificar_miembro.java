1509c9d984e317400c8eeb75acaf542eea63fa39
JulioTurren <171182@ids.upchiapas.edu.mx>
2019-06-23 12:47:44 -0500

    Lista

---
+++ Lista/app/src/main/java/com/example/lista/modificar_miembro.java
@@ -0,0 +1,13 @@
+package com.example.lista;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class modificar_miembro extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_modificar_miembro);
+    }
+}
